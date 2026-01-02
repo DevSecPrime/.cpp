b52e5527ad792684b40f037e82052e53c7553b3d
Aryan Rana <ranaaryan620@gmail.com>
2026-01-02 21:15:14 +0530

    condition even-odd

--- condition/even-odd.cpp
+++ condition/even-odd.cpp
@@ -15,7 +15,11 @@ int main()
     }
     else
     {
-        if (num % 2 == 0)
+        if (num == 0)
+        {
+            cout << "0 is not EVEN or ODD!!" << endl;
+        }
+        else if (num % 2 == 0)
         {
             cout << "Even!" << endl;
         }
