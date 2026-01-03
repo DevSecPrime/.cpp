c6c09850d287f0455df9de9735d0dedd0369d921
Aryan Rana <ranaaryan620@gmail.com>
2026-01-03 13:52:46 +0530

    hollow pyramid

---
+++ patterns/hollow-pyramid.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    // print rows
+    for (int row = 0; row < n; row++)
+    {
+        /* code */
+        for (int col = 0; col < n - row - 1; col++)
+        {
+            /* code */
+            cout << " ";
+        }
+
+        // print star
+        for (int col = 0; col < row + 1; col++)
+        {
+
+            // /* code */
+            // print star for first col and last col
+            if (col == 0 || col == row + 1 - 1)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
