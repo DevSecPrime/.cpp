b383c00e91be6c51dd884622bfc81ae8bfa570e6
Aryan Rana <ranaaryan620@gmail.com>
2026-01-08 21:34:47 +0530

    printing an array

--- arrays/demo.cpp
+++ arrays/demo.cpp
@@ -9,5 +9,16 @@ int main()
     cout << arr[2] << endl;
     cout << arr[10] << endl; // garbage value
 
+    // Printing an array
+    int a[50] = {1, 2, 3, 4, 5};
+
+    int n = 5;
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << arr[i] << " " << endl; // Here, i is working as an index
+    }
+
     return 0;
 }
