a1c660055af764a40b3db5faba905549ddd0785e
Aryan Rana <ranaaryan620@gmail.com>
2026-01-08 22:56:22 +0530

    print sum of array

--- arrays/ps1.cpp
+++ arrays/ps1.cpp
@@ -3,8 +3,24 @@ using namespace std;
 
 int main()
 {
-    int arr[10];
+    cout << "Sum of the array: " << endl;
+    int sa[5];
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        cin >> sa[i];
+    }
 
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        sum = sum + sa[i];
+    }
+
+    cout << sum << endl;
+
+    int arr[10];
     for (int i = 0; i < 10; i++)
     {
         /* code */
