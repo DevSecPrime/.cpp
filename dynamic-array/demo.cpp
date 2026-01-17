a61f908358315d2b81cee65ab95f40efb7c757de
Aryan Rana <ranaaryan620@gmail.com>
2026-01-17 20:11:59 +0530

    dynamic array

---
+++ dynamic-array/demo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void printDynamicArray(int arr[], int n)
+{
+    cout << "Output: ";
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the how much elements you want inside array: " << endl;
+    cin >> n;
+
+    //
+    cout << "Enter " << n << " elements : " << '\n';
+    int *arr = new int[n]; // initialise the value of n --> may use int arr[n]; but not valid because its ont supported by the compiler
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        int data;
+        cin >> data;
+        arr[i] = data;
+    }
+
+    printDynamicArray(arr, n);
+}
