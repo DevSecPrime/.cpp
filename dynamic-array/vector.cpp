47764cb3bc3ac331acbb754425eabf713b42fdf7
Aryan Rana <ranaaryan620@gmail.com>
2026-01-17 21:26:56 +0530

    vector array

--- dynamic-array/vector.cpp
+++ dynamic-array/vector.cpp
@@ -2,61 +2,85 @@
 #include <vector>
 using namespace std;
 
+/*
+    Function to print all elements of the vector
+*/
 void printVector(vector<int> v)
 {
-    int sizeOfVector = v.size();
+    int sizeOfVector = v.size(); // Get total number of elements
 
+    // Loop through vector and print each element
     for (int i = 0; i < sizeOfVector; i++)
     {
-        /* code */
         cout << v[i] << " ";
+        // OR
+        // cout << v.at(i) << " ";
+        // Both print the same output
     }
 }
 
 int main()
 {
-    vector<int> v;
+    vector<int> v; // Declare an empty vector
 
-    // v.push_back(1);
-    // v.push_back(5);
-    // v.push_back(7);
-    // v.push_back(10);
-    // v.push_back(25);
+    /*
+        Uncomment below lines to insert static values
+        v.push_back(1);
+        v.push_back(5);
+        v.push_back(7);
+        v.push_back(10);
+        v.push_back(25);
+    */
 
     int n;
     cout << "Enter value of n: " << endl;
     cin >> n;
+
+    /*
+        Take input from user
+        push_back() dynamically increases vector size
+    */
     for (int i = 0; i < n; i++)
     {
-        /* code */
         int data;
         cin >> data;
         v.push_back(data);
     }
-    // I can also increate the inserts, which array can't perform
+
+    /*
+        Vector allows dynamic insertion
+        This is not possible with normal arrays
+    */
     // for (int i = 0; i < 6; i++)
     // {
-
     //     v.push_back(13);
     // }
 
-    // Print before pop
+    // Print vector before pop operation
     cout << "Printing all element: " << '\n';
     printVector(v);
 
     cout << endl
          << endl;
-    // pop ---> pop/delete element from last index
+
+    /*
+        pop_back() removes the last element from vector
+    */
     v.pop_back();
 
-    cout << "Printing  with pop: " << endl;
+    // Print vector after pop operation
+    cout << "Printing with pop: " << endl;
     printVector(v);
 
     cout << endl
          << endl;
-    // clear the vector array
-    // v.clear();
-    // cout << "vector is cleared: " << '\n';
 
-    printVector(v);
+    /*
+        clear() removes all elements from vector
+        v.clear();
+        cout << "vector is cleared: " << '\n';
+        printVector(v);
+    */
+
+    return 0;
 }
